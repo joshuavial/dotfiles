53f6318bfdb9b9bd6e50d7c8e708e84d0b004288
Joshua Vial <joshua@enspiral.com>
2015-05-12 11:13:47 +1200

    adding global gitignore

--- link.sh
+++ link.sh
@@ -4,3 +4,6 @@ ln -s `pwd`/rspec ~/.rspec
 ln -s `pwd`/zshrc ~/.zshrc
 ln -s `pwd`/psqlrc ~/.psqlrc
 ln -s `pwd`/tmux.conf ~/.tmux.conf
+
+#ensure that git config --global core.excludesfile ~/.gitignore_global is setup
+ln -s `pwd`/gitignore_global ~/.gitignore_global
